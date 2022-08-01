69dc422690e9bd6932fe0ecb0e4e68bcd71cb304
TeamDmember02 <s10240069@connect.np.edu.sg>
2022-08-01 16:21:27 +0800

    hi

--- project_group/api.py
+++ project_group/api.py
@@ -1,17 +1,22 @@
 from pathlib import Path
 import csv
-import requests
+import requests, json, re
 home = Path.home()
 fp = Path.cwd()/"Cash on hand.csv"
 fp.touch()
 api_key = "8JYQTW7C6BBZ3SOQ"
-function = "FX_WEEKLY"
+function = "CURRENCY_EXCHANGE_RATE"
 from_currency = 'USD'
 to_currency ='SGD'
 url = f"https://www.alphavantage.co/query?function={function}&from_currency=USD&to_currency=SGD&apikey={api_key}"
 r = requests.get(url)
 data = r.json()
-print(data)
+data = json.dumps(data,indent=4)
+
+data = re.search('Exchange Rate": ".+',data)
+data = data.group(0)
+print(data.strip('Exchange Rate":"",'))
+
 
 
 
